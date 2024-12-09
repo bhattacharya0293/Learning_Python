7b446cd1b139b8f9eef80d3be67f3bbbeabd8185
bhattacharya0293 <bhattacharya0293@gmail.com>
2024-12-09 00:24:20 -0600

    added basics of python

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,1 +1,12 @@
-print("Hello World")
+#########################################################
+################### PYTHON BASICS #######################
+#########################################################
+
+#print a string
+print("Hello World");
+
+#print a number
+print(23);
+
+#sum of two numbers
+print(2+3);
